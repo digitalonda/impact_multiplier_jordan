c9d538343cbe22253615bc4b7b44a91f78385632
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-20 23:29:14 +0700

    up

--- app.py
+++ app.py
@@ -451,9 +451,7 @@ with st.sidebar:
       if bt:
         load_history(k)
 
-      
-      click_handle = lambda:delete_single_history(k)
-      #st.button("Delete",key="btn-history-"+str(k),on_click=click_handle)
+      st.button("Delete",key="btn-history-"+str(k),args=(k),on_click=delete_single_history)
 
   st.divider()
   delete_history = st.button("Clear History")
