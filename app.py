a11c4e06d3423d319f5b62add1ae7b844bbf0df2
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-20 23:34:17 +0700

    up

--- app.py
+++ app.py
@@ -199,16 +199,15 @@ def delete_docs(doc_id,doc_nsp="default",doc_list_nsp="list"):
         remove_selected_style_docs(doc_id)
              
 def delete_single_history(chat_id):
-    
     nsp="chat_history"
     list_nsp="chat_history_list"
     l1 = get_from_index_raw(default_vec_embedding,1000,nsp,filter={"chat_id":chat_id}) 
     d1 = [x["id"] for x in l1]
-
     
     if len(d1)>0:
         data_index.delete(d1, namespace=nsp)
     data_index.delete([str(int(chat_id))], namespace=list_nsp)
+    st.session_state.all_chat_history.pop(chat_id)
 
 if not "all_docs" in st.session_state:
     st.session_state.all_docs = {}
