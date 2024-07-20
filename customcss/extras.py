8ac4cfff84d0b3703ebcd90daf6e260ee4ecce7f
acscoder-digitalonda <acscoder@digitalonda.com>
2024-07-20 19:42:03 +0700

    up

--- customcss/extras.py
+++ customcss/extras.py
@@ -2,17 +2,21 @@ import streamlit as st
 from streamlit_extras.stylable_container import stylable_container
 
 def custom_btn(title,icon):
-    with stylable_container(
-        key="container_with_border",
-        css_styles=r"""
+    css = r"""
             button div:before {
                 font-family: 'Font Awesome 5 Free';
-                content: '\{icon}';
+                content: '\
+                    """
+    css = css + icon
+    css = css + r"""';
                 display: inline-block;
                 padding-right: 3px;
                 vertical-align: middle;
                 font-weight: 900;
             }
-            """.format(icon=icon),
+            """
+    with stylable_container(
+        key="container_with_border",
+        css_styles=css
     ):
         st.button(title)
